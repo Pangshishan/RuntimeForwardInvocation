242be30b628e199c5af7e7df247812d6fa4e8607
庞仕山 <pangshishan@aliyun.com>
2017-03-24 23:02:35 +0800

    添加注释

--- Runtime消息转发/Runtime消息转发/ViewController.m
+++ Runtime消息转发/Runtime消息转发/ViewController.m
@@ -36,7 +36,9 @@
 //+ (BOOL)resolveInstanceMethod:(SEL)sel
 //{
 //    NSLog(@"sel is %@", NSStringFromSelector(sel));
+//    // 当方法是setter方法时
 //    if (sel == @selector(setName:)) {
+//        // 动态给本类添加方法
 //        class_addMethod([SomeOtherObject class], sel, (IMP)dynamicMethodIMP, "v@:");
 //    }
 //
@@ -44,13 +46,13 @@
 //}
 
 #pragma mark - 2. 找个类处理她
-//- (id)forwardingTargetForSelector:(SEL)aSelector
-//{
-//    if ([NSStringFromSelector(aSelector) isEqualToString:@"setName:"]) {
-//        return [[SomeOtherObject alloc] init];
-//    }
-//    return [super forwardingTargetForSelector:aSelector];
-//}
+- (id)forwardingTargetForSelector:(SEL)aSelector
+{
+    if ([NSStringFromSelector(aSelector) isEqualToString:@"setName:"]) {
+        return [[SomeOtherObject alloc] init];
+    }
+    return [super forwardingTargetForSelector:aSelector];
+}
 
 #pragma mark - 3. 最后一步，把selector的所有细节包程NSInvocation, invocation对象负责分发消息给其他对象，这一步是完整的转发机制
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector
@@ -69,8 +71,11 @@
     }
     return signature; // 只有当返回值不是空的时候才能转发成功
 }
+// anInvocation里打包了此类不能处理的selector所有的细节
 - (void)forwardInvocation:(NSInvocation *)anInvocation
 {
+    // 将anInvocation里打包的消息，发送给SomeOtherObject处理，如果SomeOtherObject也不能处理，可以在它里面继续转发
+    // 这也是变相的实现了多继承，但是消息转发只是将一部分功能放到其他类，并没有多继承造成的代码过于丰富的问题
     [anInvocation invokeWithTarget:[[SomeOtherObject alloc] init]];
 }
 
@@ -78,10 +83,6 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     self.name = @"pangshishan";
-    
-    
-//    NSString *str = @"123456789";
-//    NSLog(@"=%d, =%d", [str hasPrefix:@"1234"], [str hasSuffix:@"6789"]);
 }
 
 
